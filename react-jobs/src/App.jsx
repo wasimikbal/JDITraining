df18e521e8709208ad32901050c698e812b0b7ca
wasimikbal <44136359+wasimikbal@users.noreply.github.com>
2024-05-24 21:55:24 +0500

    integrated and validated tailwind

--- react-jobs/src/App.jsx
+++ react-jobs/src/App.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div className='text-5xl'>App</div>
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
   )
 }
 
