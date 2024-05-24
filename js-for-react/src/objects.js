d2e3a5b7817772e326f8c755e5f1052aec5f1a4a
wasimikbal <44136359+wasimikbal@users.noreply.github.com>
2024-05-24 17:17:03 +0500

    Create objects.js

---
+++ js-for-react/src/objects.js
@@ -0,0 +1,10 @@
+
+const person = {
+    name: "name_of_person",
+    age: 20,
+    weight: 50
+}
+
+ const {name, age, weight} = person;
+
+ const other_person = {...person, name: "other_person", age: 23}
