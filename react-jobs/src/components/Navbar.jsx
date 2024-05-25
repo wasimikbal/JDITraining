7781cf885789c23cfac339a6e40a94c774675840
wasimikbal <44136359+wasimikbal@users.noreply.github.com>
2024-05-25 19:44:06 +0500

    Update Navbar.jsx

--- react-jobs/src/components/Navbar.jsx
+++ react-jobs/src/components/Navbar.jsx
@@ -3,7 +3,6 @@ import logo from '../assets/images/logo.png'
 
 const Navbar = () => {
   return (
-    <div>
       <nav className="bg-indigo-700 border-b border-indigo-500">
         <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
           <div className="flex h-20 items-center justify-between">
@@ -44,8 +43,6 @@ const Navbar = () => {
           </div>
         </div>
       </nav>
-
-    </div>
   )
 }
 
