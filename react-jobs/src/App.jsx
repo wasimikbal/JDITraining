73bb77d8d369fcb7d6176c7cafb13db6c7f0272f
wasimikbal <44136359+wasimikbal@users.noreply.github.com>
2024-05-26 03:38:42 +0500

    Created NotFoundPage

--- react-jobs/src/App.jsx
+++ react-jobs/src/App.jsx
@@ -8,11 +8,13 @@ import {
 import MainLayout from './layouts/MainLayout'
 import Homepage from './pages/Homepage'
 import JobsPage from './pages/JobsPage'
+import NotFoundPage from './pages/NotFoundPage'
 
 const router = createBrowserRouter(createRoutesFromElements(
   <Route path='/' element={<MainLayout />}>
     <Route path='/' element={<Homepage />} />
     <Route path='/jobs' element={<JobsPage />} />
+    <Route path='*' element={<NotFoundPage />} />
   </Route>
 ))
 
