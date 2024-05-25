1040d910d21d014399110e4733467ba169619bee
wasimikbal <44136359+wasimikbal@users.noreply.github.com>
2024-05-26 00:00:29 +0500

    Show more or less, use state hook

--- react-jobs/src/components/JobListing.jsx
+++ react-jobs/src/components/JobListing.jsx
@@ -1,37 +1,46 @@
 import React from 'react'
+import { useState } from 'react'
+import {FaMapMarker} from 'react-icons/fa'
 
-const JobListing = ({job}) => {
-  return (
-    <div className="bg-white rounded-xl shadow-md relative">
-              <div className="p-4">
+const JobListing = ({ job }) => {
+
+    const [showFullDescription, setShowFullDescription] = useState(false)
+    const description = showFullDescription ? job.description : `${job.description.substring(0, 90)} ...`
+    return (
+        <div className="bg-white rounded-xl shadow-md relative">
+            <div className="p-4">
                 <div className="mb-6">
-                  <div className="text-gray-600 my-2">{job.type}</div>
-                  <h3 className="text-xl font-bold">{job.title}</h3>
+                    <div className="text-gray-600 my-2">{job.type}</div>
+                    <h3 className="text-xl font-bold">{job.title}</h3>
                 </div>
 
                 <div className="mb-5">
-                  {job.description}
+                    {description}
                 </div>
+
+                <button onClick={() => { setShowFullDescription((prev) => !prev) }} className="text-indigo-500 mb-5 hover:text-indigo-600">
+                    {showFullDescription ? 'Less' : 'More'}
+                </button>
 
                 <h3 className="text-indigo-500 mb-2">{job.salary} / Year</h3>
 
                 <div className="border border-gray-100 mb-5"></div>
 
                 <div className="flex flex-col lg:flex-row justify-between mb-4">
-                  <div className="text-orange-700 mb-3">
-                    <i className="fa-solid fa-location-dot text-lg"></i>
-                    {job.location}
-                  </div>
-                  <a
-                    href="job.html"
-                    className="h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm"
-                  >
-                    Read More
-                  </a>
+                    <div className="text-orange-700 mb-3">
+                        <FaMapMarker className='inline mb-1.5 mr-1 text-lg'/>
+                        {job.location}
+                    </div>
+                    <a
+                        href="job.html"
+                        className="h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm"
+                    >
+                        Read More
+                    </a>
                 </div>
-              </div>
             </div>
-  )
+        </div>
+    )
 }
 
 export default JobListing
