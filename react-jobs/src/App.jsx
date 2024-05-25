baf88676f9fb545ef9c84f15fa1f30178c52a253
wasimikbal <44136359+wasimikbal@users.noreply.github.com>
2024-05-26 03:33:40 +0500

    Update App.jsx

--- react-jobs/src/App.jsx
+++ react-jobs/src/App.jsx
@@ -1,30 +1,24 @@
 import React from 'react'
-import Navbar from './components/Navbar'
-import Hero from './components/Hero'
-import HomeCards from './components/HomeCards'
-import JobListings from './components/JobListings'
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider
+} from 'react-router-dom'
+import MainLayout from './layouts/MainLayout'
+import Homepage from './pages/Homepage'
+import JobsPage from './pages/JobsPage'
+
+const router = createBrowserRouter(createRoutesFromElements(
+  <Route path='/' element={<MainLayout />}>
+    <Route path='/' element={<Homepage />} />
+    <Route path='/jobs' element={<JobsPage />} />
+  </Route>
+))
 
 const App = () => {
   return (
-    <>
-      <Navbar />
-      <Hero/>
-      <HomeCards/>
-      <JobListings/>
-      
-      
-
-      
-      <section className="m-auto max-w-lg my-10 px-6">
-        <a
-          href="jobs.html"
-          className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
-        >View All Jobs</a
-        >
-      </section>
-
-
-    </>
+    <RouterProvider router={router} />
   )
 }
 
