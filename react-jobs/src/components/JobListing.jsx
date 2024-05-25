9974d046ff06133cc8ed353fc65faf9d7efb4a81
wasimikbal <44136359+wasimikbal@users.noreply.github.com>
2024-05-26 03:33:24 +0500

    replacing a tag with Link

--- react-jobs/src/components/JobListing.jsx
+++ react-jobs/src/components/JobListing.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useState } from 'react'
 import {FaMapMarker} from 'react-icons/fa'
+import { Link } from 'react-router-dom'
 
 const JobListing = ({ job }) => {
 
@@ -31,12 +32,12 @@ const JobListing = ({ job }) => {
                         <FaMapMarker className='inline mb-1.5 mr-1 text-lg'/>
                         {job.location}
                     </div>
-                    <a
-                        href="job.html"
+                    <Link
+                        to={`/job/${job.id}`}
                         className="h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm"
                     >
                         Read More
-                    </a>
+                    </Link>
                 </div>
             </div>
         </div>
