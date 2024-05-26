1d0f70fa7f3d1ac1f6c7920e26d6e66119835fb0
wasimikbal <44136359+wasimikbal@users.noreply.github.com>
2024-05-26 05:33:29 +0500

    Fetching from mock backend, useEffect

--- react-jobs/src/components/JobListings.jsx
+++ react-jobs/src/components/JobListings.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
-import jobs from '../jobs.json'
+
+import { useState, useEffect } from 'react'
 import JobListing from './JobListing'
 
-const JobListings = ({isHome}) => {
-const jobListings = isHome ? jobs.slice(0,3) : jobs;
+const JobListings = ({ isHome }) => {
+  const [jobs, setJobs] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchJobs = async () => {
+      try {
+        const res = await fetch('http://localhost:8000/jobs');
+        const data = await res.json();
+        setJobs(data);
+      } catch (error) {
+        console.log("Error fetching data: " + error);
+      }finally{
+        setLoading(false);
+      }
+
+    }
+
+    fetchJobs();
+  }, [])
+
 
   return (
     <section className="bg-blue-50 px-4 py-10">
@@ -13,11 +32,15 @@ const jobListings = isHome ? jobs.slice(0,3) : jobs;
         </h2>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
           {
-            jobListings.map((job) => {
+            loading ? (<h2>Loading ...</h2>) : 
+            (<>
+            {jobs.map((job) => {
               return (
                 <JobListing key={job.id} job={job} />
               )
-            })
+            })}
+            </>)
+            
           }
         </div>
       </div>
