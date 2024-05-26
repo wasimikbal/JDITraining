94c2ac6c958586013d0a5d0f3b13fd775306c29a
wasimikbal <44136359+wasimikbal@users.noreply.github.com>
2024-05-26 14:24:02 +0500

    Fetching job with useLoaderData

--- react-jobs/src/App.jsx
+++ react-jobs/src/App.jsx
@@ -8,14 +8,14 @@ import {
 import MainLayout from './layouts/MainLayout'
 import HomePage from './pages/HomePage'
 import JobsPage from './pages/JobsPage'
-import JobPage from './pages/JobPage'
+import JobPage, {LoadJobData} from './pages/JobPage'
 import NotFoundPage from './pages/NotFoundPage'
 
 const router = createBrowserRouter(createRoutesFromElements(
   <Route path='/' element={<MainLayout />}>
     <Route path='/' element={<HomePage />} />
     <Route path='/jobs' element={<JobsPage />} />
-    <Route path='/jobs/:id' element={<JobPage />} />
+    <Route path='/jobs/:id' element={<JobPage/>} loader={LoadJobData} />
     <Route path='*' element={<NotFoundPage />} />
   </Route>
 ))
--- react-jobs/src/pages/JobPage.jsx
+++ react-jobs/src/pages/JobPage.jsx
@@ -1,41 +1,26 @@
 import React from 'react'
 import { useState, useEffect } from 'react'
-import { useParams } from 'react-router-dom'
+import { useParams, useLoaderData } from 'react-router-dom'
 import Spinner from '../components/Spinner'
 
 const JobPage = () => {
   const { id } = useParams();
-  const [job, setJob] = useState(null);
   const [loading, setLoading] = useState(true)
+  const job = useLoaderData();
+  setTimeout(() => setLoading(false), 1000)
 
-
-  useEffect(() => {
-    const fetchJob = async () => {
-      try {
-        const res = await fetch(`http://localhost:8000/jobs/${id}`);
-        const data = await res.json();
-        setJob(data);
-        console.log(data);
-      } catch (error) {
-        console.log('Error fetching the data :' + error);
-      }finally{
-        setLoading(false)
-      }
-      
-
-    }
-setTimeout(()=>
-  {
-    fetchJob();
-  }, 1000)
-
-  }, [])
 
   return (
-    loading ? <Spinner/> : (
+    loading ? <Spinner /> : (
       <h1>{job.title}</h1>
     )
   )
 }
 
-export default JobPage
+const LoadJobData = async ({params}) => {
+  const res = await fetch(`http://localhost:8000/jobs/${params.id}`)
+  const data = await res.json();
+  return data;
+}
+
+export {JobPage as default, LoadJobData}
