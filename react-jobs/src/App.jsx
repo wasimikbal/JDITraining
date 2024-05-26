2dec8e105b5e9af9a1145ab67f87bb8ed87c676b
wasimikbal <44136359+wasimikbal@users.noreply.github.com>
2024-05-26 17:54:19 +0500

    AddJob implementation

--- react-jobs/src/App.jsx
+++ react-jobs/src/App.jsx
@@ -3,19 +3,36 @@ import {
   Route,
   createBrowserRouter,
   createRoutesFromElements,
-  RouterProvider
+  RouterProvider,
+  json
 } from 'react-router-dom'
 import MainLayout from './layouts/MainLayout'
 import HomePage from './pages/HomePage'
 import JobsPage from './pages/JobsPage'
 import JobPage, {LoadJobData} from './pages/JobPage'
+import AddJobPage from './pages/AddJobPage'
 import NotFoundPage from './pages/NotFoundPage'
+
+const addJob = async(newJob) => {
+  const res = await fetch('http://localhost:8000/jobs',
+    {
+      mode: 'no-cors',
+      method: 'POST',
+      headers: {
+        'Content_Type': 'application/json'
+      },
+      body: JSON.stringify(newJob)
+    }
+  );
+  return;
+}
 
 const router = createBrowserRouter(createRoutesFromElements(
   <Route path='/' element={<MainLayout />}>
     <Route path='/' element={<HomePage />} />
     <Route path='/jobs' element={<JobsPage />} />
     <Route path='/jobs/:id' element={<JobPage/>} loader={LoadJobData} />
+    <Route path='/add-job' element={<AddJobPage AddJobSubmit={addJob}/>}/>
     <Route path='*' element={<NotFoundPage />} />
   </Route>
 ))
